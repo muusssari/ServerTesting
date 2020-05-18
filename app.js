11a952bf61f79e3f476d2f0cf741cd27e50df6c4
Muusssari <muussari@hotmail.com>
2020-05-18 16:32:02 +0300

    take comments and send comments between clients

--- app.js
+++ app.js
@@ -7,12 +7,32 @@ const SocketIO = require('socket.io')(Http);
 let data = {
   text: "works"
 };
+const commentList = [];
 
 SocketIO.on("connection", socket => {
   console.log("socket connected");
+  socket.on('disconnect', () => {
+    console.log("socket disconnected");
+  });
+
   socket.emit("serverData", data);
+  socket.on("NewComment", (data) => {
+    commentList.push(data);
+  });
+
+  socket.on("CheckLocalComments", (data) => {
+    if(commentList.length > 0) {
+      //send comments that user doesnt have
+      socket.emit("SendComments", commentList);
+    }else {
+      
+      //send all comments from server
+    }
+  });
 });
 
 Http.listen(3000, "0.0.0.0", () => {
   console.log("listening at :3000....");
 });
+
+//const comment = new Comment(x.comment, x.Author, x.timestamp, x.pos, x.cameraState, button, this);
