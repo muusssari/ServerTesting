45ebf4e6e6d89893a8606591afd0f3fcb0112dad
Muusssari <muussari@hotmail.com>
2020-05-19 14:49:48 +0300

    set emit and on sockets for client and server communication

--- app.js
+++ app.js
@@ -3,33 +3,53 @@ const Http = require('http').Server(Express);
 const SocketIO = require('socket.io')(Http);
 
 
-
+//Test data, so client is connected to server right
 let data = {
   text: "works"
 };
 const commentList = [];
+const connectedSockets = [];
 
 SocketIO.on("connection", socket => {
   console.log("socket connected");
+  connectedSockets.push(socket);
   socket.on('disconnect', () => {
     console.log("socket disconnected");
   });
 
   socket.emit("serverData", data);
-  socket.on("NewComment", (data) => {
+  socket.on('addCommentServer', (data) => {
     commentList.push(data);
+    sendCommentToOtherSockets(socket.id, data);
+  });
+  socket.on('addThreadServer', (data) => {
+    commentList[data.mainId].threadCommentList.push(data);
+    sendThreadToOtherSockets(socket.id, data);
   });
 
   socket.on("CheckLocalComments", (data) => {
     if(commentList.length > 0) {
       //send comments that user doesnt have
-      socket.emit("SendComments", commentList);
-    }else {
-      
-      //send all comments from server
+
+      //send all comments if user has none of them
+      socket.emit("SendMissingComments", commentList);
     }
   });
 });
+
+function sendThreadToOtherSockets(id, data) {
+  const sockets = connectedSockets.filter(x => x.id !== id);
+  sockets.map(x => {
+    x.emit('addThreadClient', data)
+  });
+}
+
+function sendCommentToOtherSockets(id, data) {
+  const sockets = connectedSockets.filter(x => x.id !== id);
+  sockets.map(x => {
+    x.emit('newCommentClient', data)
+  });
+}
 
 Http.listen(3000, "0.0.0.0", () => {
   console.log("listening at :3000....");
