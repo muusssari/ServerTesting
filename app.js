2827f59553599a69143d0575c2f4508794f6730c
Muusssari <muussari@hotmail.com>
2020-05-19 16:53:46 +0300

    test https

--- app.js
+++ app.js
@@ -1,46 +1,59 @@
 const Express = require('express')();
 const fs = require('fs');
-const Https = require('https').createServer({
+const http = require('http');
+const https = require('https');
+
+const options = {
   key: fs.readFileSync('./server.key'),
   cert: fs.readFileSync('./server.cert')
-}, Express);
-const SocketIO = require('socket.io')(Https);
-
-
-//Test data, so client is connected to server right
-let data = {
-  text: "works"
 };
+
+//const httpServer = http.createServer(Express).listen(3001);
+const httpsServer = https.createServer(credentials, Express).listen(3000, () => {console.log("https: 3000")});
+
+const SocketIO = require('socket.io');
 const commentList = [];
 const connectedSockets = [];
 
-SocketIO.on("connection", socket => {
-  console.log("socket connected");
-  connectedSockets.push(socket);
-  socket.on('disconnect', () => {
-    console.log("socket disconnected");
-    removeA(connectedSockets, socket);
-  });
+function emitNewOrder(server) {
+  const io = SocketIO.listen(server);
 
-  socket.emit("serverData", data);
-  socket.on('addCommentServer', (data) => {
-    commentList.push(data);
-    sendCommentToOtherSockets(socket.id, data);
-  });
-  socket.on('addThreadServer', (data) => {
-    commentList[data.mainId].threadCommentList.push(data);
-    sendThreadToOtherSockets(socket.id, data);
-  });
+  //Test data, so client is connected to server right
+  
 
-  socket.on("CheckLocalComments", (data) => {
-    if(commentList.length > 0) {
-      //send comments that user doesnt have
+  io.on("connection", socket => {
+    console.log("socket connected");
+    connectedSockets.push(socket);
+    socket.on('disconnect', () => {
+      console.log("socket disconnected");
+      removeA(connectedSockets, socket);
+    });
+    let testObject = {
+      text: "works"
+    };
+    socket.emit("serverData", testObject);
 
-      //send all comments if user has none of them
-      socket.emit("SendMissingComments", commentList);
-    }
+    socket.on('addCommentServer', (data) => {
+      commentList.push(data);
+      sendCommentToOtherSockets(socket.id, data);
+    });
+    socket.on('addThreadServer', (data) => {
+      commentList[data.mainId].threadCommentList.push(data);
+      sendThreadToOtherSockets(socket.id, data);
+    });
+
+    socket.on("CheckLocalComments", (data) => {
+      if(commentList.length > 0) {
+        //send comments that user doesnt have
+
+        //send all comments if user has none of them
+        socket.emit("SendMissingComments", commentList);
+      }
+    });
   });
-});
+}
+emitNewOrder(httpsServer);
+
 
 function sendThreadToOtherSockets(id, data) {
   const sockets = connectedSockets.filter(x => x.id !== id);
@@ -55,13 +68,10 @@ function sendCommentToOtherSockets(id, data) {
     x.emit('newCommentClient', data)
   });
 }
-
-Https.listen(3000, "0.0.0.0", () => {
+/*, "0.0.0.0"
+Https.listen(3000, () => {
   console.log("listening at :3000....");
-});
-
-//const comment = new Comment(x.comment, x.Author, x.timestamp, x.pos, x.cameraState, button, this);
-
+});*/
 
 function removeA(arr) {
   var what, a = arguments, L = a.length, ax;
