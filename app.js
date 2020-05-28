02ede8b70adc518df68c7e894f9d8ac0781bd590
Muusssari <muussari@hotmail.com>
2020-05-28 13:27:26 +0300

    fix line missing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const options = {
 };
 
 //HTTPS server
-const httpsServer = https.createServer(App).listen(3000, "0.0.0.0", () => {
+const httpsServer = https.createServer(App, options).listen(3000, "0.0.0.0", () => {
   console.log("http server started port: 3000");
 });
 
